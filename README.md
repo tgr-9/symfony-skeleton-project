774e7dfdcd8334a14d3c8402b3707c65a6ab649b
tgr-9 <top.gru.999@gmail.com>
2024-11-11 22:03:23 +0100

    SetupProjectConsoleCommand

--- README.md
+++ README.md
@@ -10,28 +10,14 @@ DOCKER_MYSQL_PORT=3306
 DOCKER_NGINX_PORT=8081
 ```
 
-If this a CLI-only application, remove references to PHP-FPM and Nginx:
-
-* docker/php-fpm
-* docker/nginx
-* remove services `php-fpm` & `nginx` from `docker-composer.yml`
-
-Update the database name of your application:
-
-* docker/mysql
-* .env DATABASE_URL
-
-If you don't need database functionality you can remove these references:
-
-* docker/mysql
-* .env DATABASE_URL
-* remove service `mysql` from `docker-composer.yml`
-* `make composer arg="remove doctrine/orm"`
-* `make composer arg="remove adrenalinkin/doctrine-naming-strategy"`
+```bash
+# Install dependencies
+> make composer arg="install"
+# Setup project
+make console arg="app:setup"
+```
 
 ```bash
 # Build docker containers
 > make build-containers
-# Install dependencies
-> make composer arg="install"
 ```
---
+++ src/Console/SetupProjectConsoleCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console;
+
+use League\Flysystem\FilesystemOperator;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Yaml\Yaml;
+
+#[AsCommand(name: 'app:setup')]
+final class SetupProjectConsoleCommand extends Command
+{
+    public function __construct(
+        private readonly FilesystemOperator $filesystem,
+    ) {
+        parent::__construct();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $dockerComposeYml = Yaml::parse($this->filesystem->read('docker-compose.yml'));
+
+        $io->title('Setup project');
+
+        if (!$io->confirm('Do you want to setup PHP-FPM and Nginx?', false)) {
+            $this->filesystem->deleteDirectory('docker/php-fpm');
+            $this->filesystem->deleteDirectory('docker/nginx');
+
+            unset($dockerComposeYml['services']['php-fpm']);
+            unset($dockerComposeYml['services']['nginx']);
+        }
+
+        if (!$io->confirm('Do you want to setup MySQL?', false)) {
+            $this->filesystem->deleteDirectory('docker/mysql');
+            $this->filesystem->deleteDirectory('migrations');
+            $this->filesystem->delete('config/packages/doctrine.yaml');
+            $this->filesystem->delete('config/packages/doctrine_migrations.yaml');
+
+            $process = new Process(['composer', 'remove', 'adrenalinkin/doctrine-naming-strategy']);
+            $process->run();
+
+            $process = new Process(['composer', 'remove', 'doctrine/orm']);
+            $process->run();
+
+            unset($dockerComposeYml['services']['mysql']);
+        } else {
+            $io->info('Do not forget to set the database name in ".env" and "/docker/mysql/mysql.init.sql"');
+        }
+
+        $this->filesystem->write('docker-compose.yml', Yaml::dump(
+            input: $dockerComposeYml,
+            inline: 10,
+        ));
+
+        return Command::SUCCESS;
+    }
+}
