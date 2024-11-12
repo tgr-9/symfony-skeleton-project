f44af43a1a61bb2ad926a411835fae20b2e4190e
tgr-9 <top.gru.999@gmail.com>
2024-11-12 07:17:31 +0100

    Added JsonCommandDeserializer

--- tests/Infrastructure/CQRS/Bus/RunAnOperation/RunAnOperation.php
+++ tests/Infrastructure/CQRS/Bus/RunAnOperation/RunAnOperation.php
@@ -9,12 +9,18 @@ final class RunAnOperation extends DomainCommand
     private string $notInitialized;
 
     public function __construct(
-        private readonly string $value,
+        private readonly ?string $value,
+        private readonly ?string $valueTwo = 'defaultValue',
     ) {
     }
 
-    public function getValue(): string
+    public function getValue(): ?string
     {
         return $this->value;
+    }
+
+    public function getValueTwo(): ?string
+    {
+        return $this->valueTwo;
     }
 }
